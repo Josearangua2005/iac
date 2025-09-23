67252d4c96502f5df865b1c8659024968d039f38
EdgarAntonioTorres <tedgar562@gmail.com>
2025-09-23 17:15:26 -0600

    Instancia EC2

--- actividad6/ec2.tf
+++ actividad6/ec2.tf
@@ -1,19 +1,17 @@
 locals {
-  instance_name  = "edgar-jose-server-terr"
   instance_count = 2
+  instance_name  = "act6-edgar-jose-server"
 }
 
 resource "aws_instance" "edgar-jose_server_terr" {
   count         = local.instance_count
   ami           = "ami-0ca4d5db4872d0c28"
   instance_type = var.instance_type
-
   tags = {
     Name = local.instance_name
   }
-
 }
 
 output "server_name" {
-  value = aws_instance.edgar-jose_server_terr.tags.Name
+  value = aws_instance.edgar-jose_server_terr[0].tags.Name
 }
